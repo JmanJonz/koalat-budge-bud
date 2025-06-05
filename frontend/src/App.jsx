96639522d13d7637fc0a6d75b8e232a72d280d79
JmanJonz <wjssaan@gmail.com>
2025-06-05 14:40:12 -0600

    double instead of add 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,8 +18,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count * 2)}>
           count is {count}
+          {console.log("count incrimented")}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
