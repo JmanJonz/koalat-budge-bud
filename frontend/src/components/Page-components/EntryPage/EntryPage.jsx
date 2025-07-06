f44cdc81397a9a2b7bad102276d5f9b83fcee2eb
JmanJonz <wjssaan@gmail.com>
2025-07-05 19:10:12 -0600

    first logic added to app

--- frontend/src/components/Page-components/EntryPage/EntryPage.jsx
+++ frontend/src/components/Page-components/EntryPage/EntryPage.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./EntryPage.module.css"
 
 export const EntryPage = () => {
+  const [transType, setTransType] = useState(null);
   return (
     <div className={styles.componentContainer}>
         <form className={styles.formcontainer}>
           <h2>Add New Transaction</h2>
+          <img className={styles.logo} src="/logo.png" alt="logo" />
             <section>
                 <h4>Transaction Type</h4>
                 <div className={styles.transTypeButtonsContainer}>
-                  <button className={styles.transButtons}>Income</button>
-                  <button className={styles.transButtons}>Expense</button>
+                  <button className={`${styles.transButtons} ${transType === "income" ? styles.buttonSelected : ""}`} onClick={()=>{setTransType("income")}} type='button'>Income</button>
+                  <button className={`${styles.transButtons} ${transType === "expense" ? styles.buttonSelected : ""}`} onClick={()=>{setTransType("expense")}} type='button'>Expense</button>
                 </div>
             </section>
             <section>
