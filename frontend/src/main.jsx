98f63efb2c32a6135b53bc2fd475b191da6d5a57
JmanJonz <wjssaan@gmail.com>
2025-07-05 18:38:48 -0600

    rebuilt a good chunk of ai form from scratch yeet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "./foundation.css" /* This contains the imported normalize.css for normalizing standard styles accross different browsers.  */
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
