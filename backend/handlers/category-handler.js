9706f3166f8eda78e99beccdc11920c2d72fd80c
JmanJonz <wjssaan@gmail.com>
2025-07-28 23:32:53 -0600

    I need to back up and get user authentication working as it is really crucial in all the other parts of my app so I am going to get that working on the backend and frontend before anything else now

---
+++ backend/handlers/category-handler.js
@@ -0,0 +1,8 @@
+import CategoryModel from "../models/Category-Model";
+
+// @desc    Create a new category
+// @route   POST /api/categories
+// @access  Private (User must be logged in)
+    export const createCategory = async (req, res) => {
+        
+    }
