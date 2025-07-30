28e44a0d96af1c0b9b585f5ee6945c495c170674
JmanJonz <wjssaan@gmail.com>
2025-07-29 19:18:12 -0600

    added basics of ui for login / create account page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { TranzTrakr } from './components/Page-components/TranzTrakr/TranzTrakr';
 import { TestPage } from './components/Page-components/TestPage/TestPage';
-
+import { LoginPage } from './components/Page-components/LoginPage/LoginPage.jsx';
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TranzTrakr/>}/>
-        <Route path="/test" element={<TestPage/>}/> 
+        <Route path="/LoginPage" element={<LoginPage/>}/>
       </Routes>
     </BrowserRouter>
     </>
---
+++ frontend/src/components/Page-components/LoginPage/LoginPage.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import styles from "./LoginPage.module.css"
+export const LoginPage = () => {
+  const [accountActionType, setaccountActionType] = useState("login");
+  return (
+    <div className={styles.componentContainer}>
+        <form className={styles.formcontainer}>
+          <h2>Account</h2>
+          <img className={styles.logo} src="/512ktbudgebudiconlogo.png" alt="logo" />
+            <section>
+                <div className={styles.accountActionTypeButtonsContainer}>
+                  <button className={`${styles.transButtons} ${accountActionType === "login" ? styles.buttonSelected : ""}`} onClick={()=>{setaccountActionType("login")}} type='button'>Login</button>
+                  <button className={`${styles.transButtons} ${accountActionType === "create" ? styles.buttonSelected : ""}`} onClick={()=>{setaccountActionType("create")}} type='button'>Create Account</button>
+                </div>
+            </section>
+            {accountActionType === "create"
+            ? <section>
+                <h4>Username</h4>
+                <input type="text" />
+            </section>
+            : ""}
+            <section>
+                <h4>Email</h4>
+                <input type="email" />
+            </section>
+            <section>
+                <h4>Password</h4>
+                <input type="password" />
+            </section>
+            {accountActionType === "create" ? <section>
+                <h4>Confirm Password</h4>
+                <input type="password" />
+            </section> : ""}
+        </form>
+    </div>
+  )
+}
