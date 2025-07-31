93e32728040a456031f81b352994ca4b3dd781ef
JmanJonz <wjssaan@gmail.com>
2025-07-31 13:27:05 -0600

    tried to change login page to lowercase since render runs on linux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { Button } from './components/reusable-components/Button/Button'
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { TranzTrakrPage } from './components/page-components/Tranz-Trakr-Page/Tranz-Trakr-Page';
-import { MenuPage } from './components/page-components/menu-page/Menu-Page';
-import {LoginPage} from "./components/page-components/login-page/Login-Page"
+import { TranzTrakrPage } from './components/page-components/Tranz-Trakr-Page/Tranz-Trakr-Page.jsx';
+import { MenuPage } from './components/page-components/menu-page/Menu-Page.jsx';
+import { LoginPage } from "./components/page-components/login-page/login-page.jsx"
 function App() {
   return (
     <>
