c620d85855e38c65a24ee7ddf6c9d09639c6e9cf
JmanJonz <wjssaan@gmail.com>
2025-08-02 17:16:17 -0600

    finished making individual gate singular

--- backend/gates/transaction-gate.js
+++ backend/gates/transaction-gate.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { createTransaction } from "../handlers/transaction-handler.js";
 
-const router = express.Router(); // creates an express router instance
+const TransactionGate = express.Router(); // creates an express router instance
 
 // define the post gate for creating a Transaction
     // when a POST request is made to the path this router is mounted on
     // the 'createTransaction' function from the handler will be executed. 
-        router.post("/", createTransaction);
+        TransactionGate.post("/", createTransaction);
 
 // you can add other Transaction-related gates here (eg router.get by id update etc)
 
-export default router; // export this instance of the express router to be used in server.js
+export default TransactionGate; // export this instance of the express router to be used in server.js
--- backend/gates/user-gate.js
+++ backend/gates/user-gate.js
@@ -14,10 +14,10 @@ UserGate.use((req, res, next) => {
 // define the post gate for creating a user
     // when a POST request is made to the path this router is mounted on
     // the 'createUser' function from the handler will be executed. 
-        router.post("/", createUser);
+        UserGate.post("/", createUser);
 
 // through this gate the user will attempt to login and get authenticated in our system
-    router.post("/login", authenticateUser)
+    UserGate.post("/login", authenticateUser)
 
 // you can add other user-related gates here (eg router.get by id update etc)
 
